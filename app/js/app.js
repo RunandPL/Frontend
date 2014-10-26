312089db8225285cf766732986dfc2c867783bf1
Marcin1991 <marolszak@vp.pl>
2014-10-26 14:00:55 +0000

    integracja z google maps

--- app/js/app.js
+++ app/js/app.js
@@ -1,18 +1,25 @@
 'use strict';
 
-
 // Declare app level module which depends on filters, and services
 angular.module('myApp', [
   'ngRoute',
   'myApp.filters',
   'myApp.services',
   'myApp.directives',
-  'myApp.controllers'
+  'myApp.controllers',
+  'google-maps'.ns()
 ]).
 config(['$routeProvider', function($routeProvider) {
 
   $routeProvider.when('/tag_board', {templateUrl: 'partials/tag_board.html', controller: 'TagBoardCtrl'});
   $routeProvider.when('/show_workout', {templateUrl: 'partials/workouts.html', controller: 'WorkoutCtrl'});
+  $routeProvider.when('/dummy_map', {templateUrl: 'partials/google_map.html', controller: 'WorkoutCtrl'});
   $routeProvider.otherwise({redirectTo: '/tag_board'});
 
-}]);
+}]).config(['GoogleMapApiProvider'.ns(), function (GoogleMapApi) {
+        GoogleMapApi.configure({
+            key: 'AIzaSyDYF6nmRZqnB_5lx5Ek0zRwITKkYvibZik',
+            v: '3.17',
+            libraries: 'weather,geometry,visualization'
+        });
+    }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -22,14 +22,63 @@ runAndApp.controller('TagBoardCtrl', ['$scope', function($scope) {
       ];
  }]);
 
+/*Workout Controller - provide workout details*/
 runAndApp.controller('WorkoutCtrl', ['$scope', function($scope) {
    $scope.header = 'Podgląd treningu';     
 }]);
 
-
+/*Slider Controler - provide slides content*/
 runAndApp.controller('SliderCtrl', function($scope, $http) {
   $http.get('slider/slides.json').success(function(data) {
     $scope.slides = data;
   });
 });
 
+/*Google Map Controller - provide dummy Google Map*/
+runAndApp.controller('mainCtrl',['$scope','GoogleMapApi'.ns(), function($scope, GoogleMapApi) {
+  
+        $scope.map = {center: {latitude: 54.3600, longitude: 18.639 }, zoom: 12, bounds: {}};
+        $scope.options = {scrollwheel: false};
+        var createRandomMarker = function (i, bounds, idKey) {
+            var lat_min = bounds.southwest.latitude,
+                lat_range = bounds.northeast.latitude - lat_min,
+                lng_min = bounds.southwest.longitude,
+                lng_range = bounds.northeast.longitude - lng_min;
+
+            if (idKey == null) {
+                idKey = "id";
+            }
+
+            var latitude = lat_min + (Math.random() * lat_range);
+            var longitude = lng_min + (Math.random() * lng_range);
+            var ret = {
+                latitude: latitude,
+                longitude: longitude,
+                title: 'm' + i
+            };
+            ret[idKey] = i;
+            return ret;
+        };
+        $scope.randomMarkers = [];
+        // Get the bounds from the map once it's loaded
+        $scope.$watch(function() { return $scope.map.bounds; }, function(nv, ov) {
+            // Only need to regenerate once
+            if (!ov.southwest && nv.southwest) {
+                var markers = [];
+                for (var i = 0; i < 50; i++) {
+                    markers.push(createRandomMarker(i, $scope.map.bounds))
+                }
+                $scope.randomMarkers = markers;
+            }
+        }, true);
+        
+        /*
+        * GoogleMapApi is a promise with a
+        * then callback of the google.maps object
+        *   @pram: maps = google.maps
+        */
+        GoogleMapApi.then(function(maps) {
+              console.log("Załadowano mape.");
+        });
+        
+    }]);
